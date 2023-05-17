0a3bcf60dd62ed748fe9c7247281b423702ea777
Blue-Gamer48 <97996264+Blue-Gamer48@users.noreply.github.com>
2023-05-17 11:59:19 +0200

    token load edit
    the token is now externlal in the config.json

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+import traceback
+
 import telegram
-from telegram import ChatPermissions
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import *
+from telegram.ext import *
 import logging
 import random
+import json
 import asyncio
+from collections import namedtuple
+def get(file):
+    try:
+        with open(file, encoding='utf8') as data:
+            return json.load(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+    except AttributeError:
+        raise AttributeError("Unknown argument")
+    except FileNotFoundError:
+        raise FileNotFoundError("JSON file wasn't found")
+
+
+def traceback_maker(err, advance: bool = True):
+    _traceback = ''.join(traceback.format_tb(err.__traceback__))
+    error = ('```py\n{1}{0}: {2}\n```').format(type(err).__name__, _traceback, err)
+    return error if advance else f"{type(err).__name__}: {err}"
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+config = get("config.json")
 logger = logging.getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("test")
@@ -29,7 +47,7 @@ async def hack(update: Update,context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Accessing Telegram Files... [▓▓▓▓▓ ]")
     await asyncio.sleep(2)
 
-app = ApplicationBuilder().token("6138874795:AAF2itswGW-mjSgcp6AeCJxboDRQkaz0ST8").build()
+app = ApplicationBuilder().token(config.token).build()
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("coinflip", coinflip))
